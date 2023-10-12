9af70a18d170b56ca6dbf14b06d5f0c25c43a77a
ilsubyeega <ilsubyeega@outlook.com>
2023-10-12 18:41:48 +0900

    Initial commit

---
+++ src/lib/sections/HelloWorld.stories.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  import HelloWorld from './HelloWorld.svelte';
+
+  export const meta = {
+    title: "Sections/Hello World",
+    component: HelloWorld
+  }
+</script>
+
+<script>
+    import '../../app.css'; // TODO: this should be imported in preview.js
+	import { Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Story name="Default">
+	<HelloWorld />
+</Story>
